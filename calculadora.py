963427c97419b932ba230d90de4a37bf38d4ee07
Jeison Palacios <149409822+Jeison6318@users.noreply.github.com>
2025-03-05 20:34:59 -0500

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,10 @@
 print(´bienvenidos a la calculadora´)
+def suma (a, b):
+  return a + b
+
+print (f"la suma de 2 + 3 es: {suma(2, 3)}")
+
+def resta (a, b):
+  return a - b
+
+print (f"la resta de 2 - 3 es: {resta(2, 3)}")
