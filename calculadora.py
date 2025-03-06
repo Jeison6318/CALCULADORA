f747fd0bd237bd04cb3cbeedb152095538fc5aa9
Jeison Palacios <149409822+Jeison6318@users.noreply.github.com>
2025-03-05 20:40:47 -0500

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -8,3 +8,13 @@ def resta (a, b):
   return a - b
 
 print (f"la resta de 2 - 3 es: {resta(2, 3)}")
+
+def multiplicacion (a, b):
+  return a * b
+
+print (f"la multiplicacion de 2 * 3 es: {multiplicacion(2, 3)}")
+
+def division (a, b):
+  return a / b
+
+print (f"la division de 2 / 3 es: {division(2, 3)}")
